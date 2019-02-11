8fed0ecf4af2435233ef86d4f0943b0fc427f759
Helge Klimek <helge.klimek@starfinanz.de>
2019-02-11 09:36:00 +0100

    no message

--- src/main/java/com/example/zadscraper/Main.java
+++ src/main/java/com/example/zadscraper/Main.java
@@ -1,4 +1,9 @@
-package com.example.zadscraper
+package main.java.com.example.zadscraper;
 
+/**
+ * @author hek
+ * Copyright 2019 by Star Finanz-Software Entwicklungs und Vertriebs GmbH
+ * Created on 2019-02-11
+ */
 public class Main {
 }
