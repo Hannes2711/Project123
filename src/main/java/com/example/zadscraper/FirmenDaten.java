106be5dbfb6d9aa0c874b76d953c32af24fa8236
BlairBass <43165477+BlairBass@users.noreply.github.com>
2019-02-12 15:00:50 +0100

    Hallo

---
+++ src/main/java/com/example/zadscraper/FirmenDaten.java
@@ -0,0 +1,28 @@
+package com.example.zadscraper;
+
+import java.util.Date;
+
+public class FirmenDaten {
+
+  String name = null;
+  String KontoinformationsdiensteK = null;
+  String ZahlungsauslösediensteZ = null;
+
+  public FirmenDaten(String name, String kontoinformationsdiensteK, String zahlungsauslösediensteZ) {
+    this.name = name;
+    KontoinformationsdiensteK = kontoinformationsdiensteK;
+    ZahlungsauslösediensteZ = zahlungsauslösediensteZ;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getKontoinformationsdiensteK() {
+    return KontoinformationsdiensteK;
+  }
+
+  public String getZahlungsauslösediensteZ() {
+    return ZahlungsauslösediensteZ;
+  }
+}
